d76eb2c3f911c073beaf40b24c4f3c1303fc5c4c
Haddle <94085928+hadll@users.noreply.github.com>
2024-06-27 02:07:27 +1000

    code should be done just fixing animations

--- lyric_manager.gd
+++ lyric_manager.gd
@@ -1,6 +1,8 @@
 extends Label
+class_name Lyric
 
 var lyric:String
+var stage = 0
 
 func _ready():
 	$AnimationPlayer.play("text_prepare")
@@ -18,3 +20,12 @@ func play_remove():
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "text_remove":
 		queue_free()
+
+func play_next():
+	if stage == 0:
+		play_active()
+	if stage == 1:
+		play_out()
+	if stage == 2:
+		play_remove()
+	stage += 1
--- lyric_queue.gd
+++ lyric_queue.gd
@@ -2,15 +2,37 @@ extends Control
 
 var lyric = preload("res://lyric.tscn")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+var lyrics = {"0:1.00":"Wawa", "0:3.0":"wewo", "0:4.0":"testing"}
+var line = -1
+var time = 0
 
 func create_line(text:String):
 	var new_line = lyric.instantiate()
-	
+	new_line.lyric = text
+	%Lyric_bar.add_child(new_line)
+
+func _ready():
+	_on_timer_timeout()
+
+func _on_timer_timeout():
+	line += 1
+	if line < lyrics.size():
+		var time_to_add = string_to_seconds(lyrics.keys()[line])
+		$Timer.start(time_to_add-time)
+		time += time_to_add
+		
+		if line -1 >= 0:
+			create_line(lyrics.values()[line-1])
+	for item in %Lyric_bar.get_children():
+		if item is Lyric:
+			item.play_next()
+
+func string_to_seconds(time_string: String) -> float:
+	var parts = time_string.split(":")
+	var minutes = parts[0].to_int()
+	var seconds_and_milliseconds = parts[1].split(".")
+	var seconds = seconds_and_milliseconds[0].to_int()
+	var milliseconds = seconds_and_milliseconds[1].to_int()
+	var total_seconds = minutes * 60 + seconds + milliseconds / 1000.0
+	return total_seconds
+
